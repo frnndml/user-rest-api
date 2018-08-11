1bf699d713723cc064ba96161d61d3f6b8bb658b
Fernando Melo <fernando.o.melo@gmail.com>
2018-08-11 12:41:10 -0300

    add missing fields

--- src/main/java/com/user/model/User.java
+++ src/main/java/com/user/model/User.java
@@ -7,6 +7,10 @@ public class User {
 	@Id
 	private String id;
 	private String name;
+	private String email;
+	private String password;
+	private String address;
+	private String phone;
 	private String profile;
 
 	public String getId() {
@@ -23,6 +27,38 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public String getProfile() {
